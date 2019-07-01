0811176d1a39ad07eb3d9aec2496e290641b3384
Mohannad Alaa <MAlaa@STA.com>
2019-07-01 15:59:08 +0200

    Add project files.

---
+++ QRScannerPOC/QRScannerPOC.Android/Services/QRScannerService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using QRScannerPOC.DependencyServices;
+using System.Threading.Tasks;
+using ZXing.Mobile;
+
+[assembly: Dependency(typeof(QRScannerPOC.Droid.Services.QRScannerService))]
+namespace QRScannerPOC.Droid.Services
+{
+    class QRScannerService : IQRScannerService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ QRScannerPOC/QRScannerPOC/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using QRScannerPOC.DependencyServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+
+namespace QRScannerPOC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Generate_Barcode(object sender, EventArgs e)
+        {
+            BarcodeImageView.BarcodeValue = BCString.Text;
+            BarcodeImageView.IsVisible = true;
+        }
+
+        //Using Dependency Service .. Stable
+        private async void Scan_QRcode_FirstWay_DS(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQRScannerService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    resultt.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        //Using built-in Library Component ..
+       public void Scan_QRcode_2ndway(object sender, EventArgs e)
+        {
+            BarcodeImageView.IsVisible = false;
+            //BarcodeScanView.IsVisible = true;
+            //BarcodeScanView.IsScanning = true;
+        }
+
+        public void Handle_OnScanResult(Result result)
+        {
+            if (string.IsNullOrWhiteSpace(result.Text))
+                return;
+            resultt.Text = result.Text;
+
+        }
+
+    }
+}
